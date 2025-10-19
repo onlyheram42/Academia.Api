1c0e6af89508bce799ed4f108d4c9105bf7f1e01
Hemanth Sriram <hsr.gandi@gmail.com>
2025-10-19 23:13:18 +0530

    project init

---
+++ Academia.Api/Academia.Infrastructure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Academia.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Academia.Api/Academia.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Academia.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
